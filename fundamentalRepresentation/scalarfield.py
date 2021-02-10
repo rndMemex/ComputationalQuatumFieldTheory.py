e9a850eeeae302a42428007dc5b48a7121656ad0
rndmemex <rndmemex@protonmail.com>
2021-02-10 16:05:52 +0000

    DxD Matrix Sab fundamental  representation

--- fundamentalRepresentation/scalarfield.py
+++ fundamentalRepresentation/scalarfield.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib as plt
-from sympy import symbols
 from sympy import Matrix
-from sympy.physics.quantum import AntiCommutator
 from sympy.physics.quantum import Operator, Dagger
+from sympy.matrices import zeros
+from fractions import Fraction
 
 
 #this is a script with default vacum expectation valu 1/root(2) x [0, 0, v]T, v ∈ R
@@ -47,24 +47,25 @@ const_8 =np.sqrt(1/3)
 
 
 vev =  np.transpose(Matrix([[0, 0, 1*np.sqrt(1/2)]]))
-print(λ_8)
 
 # calculate DxD= S_ab
-# # define an array of matrices or list 
+# define an array of matrices or list 
 λ_list = [λ_1, λ_2, λ_3, λ_4, λ_5, λ_6, λ_7, λ_8]
-i=0 
-j=0
+# S_ab = np.zeros((8,8))  using numpy
+S_ab = zeros(8,8)
+i,j=0,0
 while i < len(λ_list):
-        # print("λ_list[i]",λ_list[i])  
-        # print("λ_list[j]",λ_list[j])
         anti_commutator = np.dot(λ_list[i],λ_list[j]) +   np.dot(λ_list[j],λ_list[i])          
         Sab =  np.dot(Dagger(vev),np.dot(anti_commutator,vev))
-        print('S_{}{}:{}' . format( i+1 , j+1, Sab))
+        # print('S_{}{}:{}' . format( i+1 , j+1, Sab))
+        S_ab[i,j] = Sab
         j +=1
         if j == len(λ_list):
             j=0
-            i+=1   
+            i+=1  
 
+
+print("S_ab matrix:{}" . format(S_ab))
 
 
 
